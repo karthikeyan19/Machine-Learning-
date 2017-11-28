2f10b5b13e65eda5599513abe99380fcfa321109
Karthikeyan A <karthikeyana97@gmail.com>
2017-11-28 19:05:00 +0530

    support vector machine

---
+++ Support Vector Machine/svmClassifier.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn  import preprocessing, cross_validation, neighbors, svm
+import pandas as pd
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['Class'], 1))
+y = np.array(df['Class'])
+
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+example_measures = np.array([[4,1,2,1,3,1,2,5,2]])
+example_measures = example_measures.reshape(len(example_measures),-1)
+prediction = clf.predict(example_measures)
+print(prediction)
